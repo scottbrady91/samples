0b8bde09f03fa7c1daaa20a5577594ccdd4a3e62
Scott Brady <scott.brady@rocksolidknowledge.com>
2016-05-15 22:14:30 +0100

    OWIN Basic Authentication web API example

---
+++ OwinBasicAuthentication/WebApi/Controllers/SecuredController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    public class SecuredController : ApiController
+    {
+        [HttpGet]
+        [Route("Secured")]
+        public IHttpActionResult Get()
+        {
+            return Json("Authenticated!");
+        }
+    }
+}
---
+++ OwinBasicAuthentication/WebApi/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using Thinktecture.IdentityModel.Owin;
+using WebApi;
+
+[assembly: OwinStartup(typeof (Startup))]
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            app.UseBasicAuthentication(new BasicAuthenticationOptions("SecureApi",
+                async (username, password) => await Authenticate(username, password)));
+
+            app.UseWebApi(config);
+        }
+
+        private async Task<IEnumerable<Claim>> Authenticate(string username, string password)
+        {
+            // authenticate user
+            if (username == password)
+            {
+                return new List<Claim>
+                {
+                    new Claim("name", username)
+                };
+            }
+
+            return null;
+        }
+    }
+}
