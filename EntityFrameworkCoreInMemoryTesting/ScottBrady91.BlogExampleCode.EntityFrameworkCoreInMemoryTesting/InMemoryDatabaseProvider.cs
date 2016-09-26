200382e784ab4170a586656a8856c6f592db8181
Scott Brady <scott.brady@rocksolidknowledge.com>
2016-09-26 21:04:43 +0100

    Entity Framework Core in-memory database testing

---
+++ EntityFrameworkCoreInMemoryTesting/ScottBrady91.BlogExampleCode.EntityFrameworkCoreInMemoryTesting/InMemoryDatabaseProvider.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace ScottBrady91.BlogExampleCode.EntityFrameworkCoreInMemoryTesting
+{
+    public class InMemoryDatabaseProvider
+    {
+        private readonly DbContextOptions<TestDbContext> options;
+
+        public InMemoryDatabaseProvider()
+        {
+            var builder = new DbContextOptionsBuilder<TestDbContext>();
+            builder.UseInMemoryDatabase();
+            options = builder.Options;
+        }
+
+        [Fact]
+        public void CanAddEntity()
+        {
+            var testEntity = new TestEntity {Id = 1, Name = "Test Entity"};
+
+            using (var context = new TestDbContext(options))
+            {
+                context.TestEntities.Add(testEntity);
+                context.SaveChanges();
+            }
+
+            TestEntity foundEntity;
+            using (var context = new TestDbContext(options))
+            {
+                foundEntity = context.TestEntities.FirstOrDefault(x => x.Id == testEntity.Id);
+            }
+
+            Assert.NotNull(foundEntity);
+            Assert.Equal(testEntity.Name, foundEntity.Name);
+        }
+    }
+}
---
+++ EntityFrameworkCoreInMemoryTesting/ScottBrady91.BlogExampleCode.EntityFrameworkCoreInMemoryTesting/TestDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ScottBrady91.BlogExampleCode.EntityFrameworkCoreInMemoryTesting
+{
+    public class TestDbContext : DbContext
+    {
+        public TestDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<TestEntity> TestEntities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TestEntity>(entity =>
+            {
+                entity.ToTable("TestEntites");
+                entity.HasKey(x => x.Id);
+                entity.Property(x => x.Name).HasMaxLength(200).IsRequired();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class TestEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
