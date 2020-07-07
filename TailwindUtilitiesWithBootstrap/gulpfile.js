e3156c7b4d738cd7f3551d8ed73a295e4e8b524b
Scott Brady <scott@scottbrady91.com>
2020-07-07 19:29:38 +0100

    Added purgecss

--- TailwindUtilitiesWithBootstrap/gulpfile.js
+++ TailwindUtilitiesWithBootstrap/gulpfile.js
@@ -1,12 +1,17 @@
 const { src, dest } = require('gulp')
 const concat = require('gulp-concat')
 const postcss = require('gulp-postcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
 
 function tailwind() {
     return src('styles.css')
         .pipe(postcss([
             require('tailwindcss'),
-            require('autoprefixer')
+            require('autoprefixer'),
+            purgecss({
+                content: ['./*.html'],
+                defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+            })
         ]))
         .pipe(concat('tailwind.css'))
         .pipe(dest('./css'))
