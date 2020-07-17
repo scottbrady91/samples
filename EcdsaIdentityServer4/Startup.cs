87147e2c2a924ae800883fea0ff55e72cebecd92
Scott Brady <scott@scottbrady91.com>
2020-07-17 19:15:08 +0100

    Initial IdentityServer4 v4

---
+++ EcdsaIdentityServer4/Startup.cs
@@ -0,0 +1,47 @@
+﻿using IdentityServerHost.Quickstart.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EcdsaIdentityServer4
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer(options =>
+                {
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
+
+                    // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                    options.EmitStaticAudienceClaim = true;
+                })
+                .AddTestUsers(TestUsers.Users);
+
+            // in-memory, code config
+            builder.AddInMemoryIdentityResources(Config.IdentityResources);
+            builder.AddInMemoryApiScopes(Config.ApiScopes);
+            builder.AddInMemoryClients(Config.Clients);
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            
+            app.UseIdentityServer();
+            app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+        }
+    }
+}
