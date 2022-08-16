3b8443019edbace7d2afcebc4c4ac07f821fa282
Scott Brady <scott@scottbrady91.com>
2022-08-16 14:51:19 +0100

    Updated JWE to use ECDSA

--- Jwe/Jwe.cs
+++ Jwe/Jwe.cs
@@ -12,15 +12,15 @@ public class Jwe
     public void CreateAndValidateJwe()
     {
         var encryptionKey = RSA.Create(3072); // public key for encryption, private key for decryption
-        var signingKey = RSA.Create(3072); // private key for signing, public key for validation
+        var signingKey = ECDsa.Create(ECCurve.NamedCurves.nistP256); // private key for signing, public key for validation
 
         var encryptionKid = Guid.NewGuid().ToString("N");
         var signingKid = Guid.NewGuid().ToString("N");
 
         var privateEncryptionKey = new RsaSecurityKey(encryptionKey) {KeyId = encryptionKid};
         var publicEncryptionKey = new RsaSecurityKey(encryptionKey.ExportParameters(false)) {KeyId = encryptionKid};
-        var privateSigningKey = new RsaSecurityKey(signingKey) {KeyId = signingKid};
-        var publicSigningKey = new RsaSecurityKey(signingKey.ExportParameters(false)) {KeyId = signingKid};
+        var privateSigningKey = new ECDsaSecurityKey(signingKey) {KeyId = signingKid};
+        var publicSigningKey = new ECDsaSecurityKey(ECDsa.Create(signingKey.ExportParameters(false))) {KeyId = signingKid};
 
         var token = CreateJwe(privateSigningKey, publicEncryptionKey);
         DecryptAndValidateJwe(token, publicSigningKey, privateEncryptionKey).Should().BeTrue();
@@ -32,12 +32,12 @@ public class Jwe
 
         var tokenDescriptor = new SecurityTokenDescriptor
         {
-            Audience = "you",
-            Issuer = "me",
-            Claims = new Dictionary<string, object>{{"sub", "scott"}},
+            Audience = "api1",
+            Issuer = "https://idp.example.com",
+            Claims = new Dictionary<string, object>{{"sub", "811e790749a24d8a8f766e1a44dca28a"}},
             
             // private key for signing
-            SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.RsaSha256),
+            SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.EcdsaSha256),
             
             // public key for encryption
             EncryptingCredentials = new EncryptingCredentials(encryptionKey, SecurityAlgorithms.RsaOAEP, SecurityAlgorithms.Aes256CbcHmacSha512)
@@ -54,8 +54,8 @@ public class Jwe
             token,
             new TokenValidationParameters
             {
-                ValidAudience = "you",
-                ValidIssuer = "me",
+                ValidAudience = "api1",
+                ValidIssuer = "https://idp.example.com",
 
                 // public key for signing
                 IssuerSigningKey = signingKey,
---
+++ Jwe/README.md
@@ -0,0 +1,5 @@
+# JWE in C#
+
+Example code from "[JSON Web Encryption (JWE) in .NET](https://www.scottbrady91.com/c-sharp/json-web-encryption-jwe-in-dotnet-core)", targeting .NET 6.
+
+To learn more about JWE, check out my other article "[Understanding JWE](https://www.scottbrady91.com/jose/json-web-encryption)".
