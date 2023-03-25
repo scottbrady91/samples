8655b93fbb604e729a452e24f1eb862d02a5f19e
Scott Brady <scott@scottbrady91.com>
2023-03-25 15:16:50 +0000

    ECDSA cleanup

--- EcdsaKeyLoading/EcdsaKeyLoading.cs
+++ EcdsaKeyLoading/EcdsaKeyLoading.cs
@@ -16,8 +16,7 @@ namespace EcdsaKeyLoading
         [Fact] // OPTION 1: Have .NET generate an EC key for you
         public void CreateNewKey()
         {
-            var key = ECDsa.Create(ECCurve.NamedCurves.nistP256);
-            
+            using var key = ECDsa.Create(ECCurve.NamedCurves.nistP256);
             Verify(key);
         }
         
@@ -49,7 +48,7 @@ namespace EcdsaKeyLoading
                 _ => throw new NotSupportedException()
             };
 
-            var key = ECDsa.Create(new ECParameters
+            using var key = ECDsa.Create(new ECParameters
             {
                 Curve = curve,
                 D = Base64UrlEncoder.DecodeBytes(d), // optional private key
@@ -68,7 +67,7 @@ namespace EcdsaKeyLoading
         {
             var cert = new CertificateRequest("cn=Test", ECDsa.Create(ECCurve.NamedCurves.nistP256), HashAlgorithmName.SHA256)
                 .CreateSelfSigned(DateTime.UtcNow.AddDays(-2), DateTime.UtcNow.AddDays(2));
-            var key = cert.GetECDsaPrivateKey();
+            using var key = cert.GetECDsaPrivateKey();
             
             Verify(key);
         }
@@ -79,10 +78,15 @@ namespace EcdsaKeyLoading
             const string privateKey = "c711e5080f2b58260fe19741a7913e8301c1128ec8e80b8009406e5047e6e1ef";
             const string publicKey = "04e33993f0210a4973a94c26667007d1b56fe886e8b3c2afdd66aa9e4937478ad20acfbdc666e3cec3510ce85d40365fc2045e5adb7e675198cf57c6638efa1bdb";
             
+#if NET5_0_OR_GREATER
+            var privateKeyBytes = Convert.FromHexString(privateKey);
+            var publicKeyBytes = Convert.FromHexString(publicKey);
+#else
             var privateKeyBytes = FromHexString(privateKey);
             var publicKeyBytes = FromHexString(publicKey);
-
-            var key = ECDsa.Create(new ECParameters
+#endif
+            
+            using var key = ECDsa.Create(new ECParameters
             {
                 Curve = ECCurve.NamedCurves.nistP256,
                 D = privateKeyBytes,
@@ -92,17 +96,17 @@ namespace EcdsaKeyLoading
                     Y = publicKeyBytes.Skip(33).ToArray()
                 }
             });
-            
+
             Verify(key);
         }
         
         private void Verify(ECDsa key)
         {
-            // create signature
+            // create signature (ECDSA P-256 with SHA-256)
             var signature = key.SignData(data, HashAlgorithmName.SHA256);
             
             // validate signature with public key
-            var pubKey = ECDsa.Create(key.ExportParameters(false));
+            using var pubKey = ECDsa.Create(key.ExportParameters(false));
             pubKey.VerifyData(data, signature, HashAlgorithmName.SHA256).Should().BeTrue();
         }
         
